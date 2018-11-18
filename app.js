b0bc839fae6152567373ceeb3ce7f29324d7ae15
Daniel Sousa <daniel.monteiro.sousa@gmail.com>
2018-11-18 18:58:37 +0000

    added base euromillions page

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/euromillions', require('./routes/euromillions'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/angularModules/euromillions.js
+++ public/javascripts/angularModules/euromillions.js
@@ -1,10 +1,10 @@
-var app = angular.module('index', []);
+var app = angular.module('euromillions', []);
 
-app.controller('mainCtrl', function($scope) {
-
+app.controller('euromillionsCtrl', function($scope)
+{
     var date = new Date();
     var day = date.getDate() < 10 ? "0" + date.GetDate : date.getDate();
     var month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
     
-    $scope.date = day + "-" + month + "-" + date.getFullYear();
+    $scope.date = day + "-" + month + "-" + date.getFullYear(); 
 });
