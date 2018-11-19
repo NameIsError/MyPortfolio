f2ff3dc003643ccf0644e05dd402946bf07bea74
Daniel Sousa <daniel.monteiro.sousa@gmail.com>
2018-11-19 21:32:49 +0000

    main structure

--- public/javascripts/angularModules/euromillions.js
+++ public/javascripts/angularModules/euromillions.js
@@ -1,4 +1,5 @@
 var app = angular.module('euromillions', []);
+var mysql = require('mysql');
 
 app.controller('euromillionsCtrl', function($scope)
 {
@@ -7,4 +8,26 @@ app.controller('euromillionsCtrl', function($scope)
     var month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
     
     $scope.date = day + "-" + month + "-" + date.getFullYear(); 
+
+    var con = mysql.createConnection(
+        {
+            host: 'localhost',
+            user: 'root',
+            password: 'FS7312kp20122017'
+        }
+    );
+
+    con.connect(function(err)
+    {
+        if(err) throw err;
+
+        var sqlQuery = 'SELECT * FROM testtable;';
+
+        con.query(sqlQuery, function(err, result)
+        {
+            if(err) throw err;
+
+            console.log("Result: " + result);
+        })
+    });
 });
